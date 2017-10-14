a60bf21aa74198c4181ff81f3f909269894755b2
Gideon Walker <gideonw14@gmail.com>
2017-10-14 17:49:18 -0500

    - made some minor fixes to game

---
+++ game/urls.py
@@ -0,0 +1,10 @@
+from django.urls import reverse_lazy
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.password_validation import password_validators_help_text_html as pass_help
+from . import views
+
+app_name = 'game'
+urlpatterns = [
+    url(r'^$', views.index, name='index')
+]
