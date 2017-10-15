e0ac4fac2619d81a4ba5dd6c7a6041a29fa44968
Gideon Walker <gideonw14@gmail.com>
2017-10-15 14:03:50 -0500

    added login required to game

--- game/views.py
+++ game/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Task
 import json
 import os
+from django.contrib.auth.decorators import login_required
+
 
 def getTaskList():
 	all_tasks = list()
@@ -12,6 +14,7 @@ def getTaskList():
 		all_tasks.append(Task(task_data))
 	return all_tasks
 
+@login_required()
 def game(request):
     if request.method == "POST":
         print("\n\n\n", request.POST.getlist('toSubmit[]'))
